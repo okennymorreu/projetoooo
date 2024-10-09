e1bff443312c6076c5bf2b1fd1dd1495c66d2074
okennymorreu <142039412+okennymorreu@users.noreply.github.com>
2024-10-09 08:10:32 -0300

    t

--- pergunta.js
+++ pergunta.js
@@ -1,12 +1,12 @@
 criaCartao(
     'matematica',
     'como se calcula a soma dos termos de uma P.A? ',
-    'utiliza-se a fórmula: ((a1+an))*r/2'
+    'utiliza-se a fórmula: ((a1+an)*r)/2'
 )
 
 criaCartao(
     'matematica',
     'como se calcula a soma dos termos de uma P.A? ',
-    'utiliza-se a fórmula: ((a1+an))*r/2'
+    'utiliza-se a fórmula: ((a1+an)*r)/2'
 )
 //Ao todo são 20 pergumtas que exijam memorização
