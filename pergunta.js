6a1d68e701f959eb940e0464b07cf66ad0cfe1a5
okennymorreu <142039412+okennymorreu@users.noreply.github.com>
2024-10-09 08:14:26 -0300

    f

--- pergunta.js
+++ pergunta.js
@@ -5,8 +5,122 @@ criaCartao(
 )
 
 criaCartao(
+    'português',
+    'o que foi o periodo do romantismo?',
+    'Movimento artístico e cultural, da Europa'
+)
+
+criaCartao(
+    'história',
+    'Como aconteceu ',
+    'utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+
+criaCartao(
+    'matematica',
+    'como se calcula a soma dos termos de uma P.A? ',
+    'utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+
+criaCartao(
+    'matematica',
+    'como se calcula a soma dos termos de uma P.A? ',
+    'utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+
+criaCartao(
+    'matematica',
+    'como se calcula a soma dos termos de uma P.A? ',
+    'utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+criaCartao(
+    'matematica',
+    'como se calcula a soma dos termos de uma P.A? ',
+    'utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+
+criaCartao(
+    'matematica',
+    'como se calcula a soma dos termos de uma P.A? ',
+    'utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+
+criaCartao(
+    'matematica',
+    'como se calcula a soma dos termos de uma P.A? ',
+    'utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+
+criaCartao(
+    'matematica',
+    'como se calcula a soma dos termos de uma P.A? ',
+    'utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+
+criaCartao(
+    'matematica',
+    'como se calcula a soma dos termos de uma P.A? ',
+    'utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+
+criaCartao(
+    'matematica',
+    'como se calcula a soma dos termos de uma P.A? ',
+    'utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+
+criaCartao(
+    'matematica',
+    'como se calcula a soma dos termos de uma P.A? ',
+    'utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+
+criaCartao(
+    'matematica',
+    'como se calcula a soma dos termos de uma P.A? ',
+    'utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+
+criaCartao(
+    'matematica',
+    'como se calcula a soma dos termos de uma P.A? ',
+    'utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+
+criaCartao(
+    'matematica',
+    'como se calcula a soma dos termos de uma P.A? ',
+    'utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+
+criaCartao(
+    'matematica',
+    'como se calcula a soma dos termos de uma P.A? ',
+    'utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+
+criaCartao(
+    'matematica',
+    'como se calcula a soma dos termos de uma P.A? ',
+    'utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+
+criaCartao(
+    'matematica',
+    'como se calcula a soma dos termos de uma P.A? ',
+    'utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+
+criaCartao(
+    'matematica',
+    'como se calcula a soma dos termos de uma P.A? ',
+    'utiliza-se a fórmula: ((a1+an)*r)/2'
+)
+
+criaCartao(
     'matematica',
     'como se calcula a soma dos termos de uma P.A? ',
     'utiliza-se a fórmula: ((a1+an)*r)/2'
 )
 //Ao todo são 20 pergumtas que exijam memorização
+// do seu ensino médio (qualquer disciplina)
