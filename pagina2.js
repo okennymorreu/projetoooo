7cf938de223de171b57625e84f0f8da91dd77c9b
okennymorreu <142039412+okennymorreu@users.noreply.github.com>
2024-10-25 09:35:38 -0300

    d

--- pagina2.js
+++ pagina2.js
@@ -15,3 +15,7 @@ function criaCartao (categoria,pergunta,resposta){
 
 container. appendChild(cartao);
 }
+
+function mostrarResposta(card) {
+    const resposta = card.queryelector ('.resposta-cartao');
+}
